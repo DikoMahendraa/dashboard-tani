2d6bb65af4acc64a4c16688c4acd42e983ae0778
Diko <diko.dev99@gmail.com>
2024-01-29 19:41:00 +0700

    fix: initial project & removed unused features

---
+++ src/features/user/components/TemplatePointers.js
@@ -0,0 +1,18 @@
+function TemplatePointers() {
+  return (
+    <>
+      <h1 className="text-2xl mt-8 font-bold">Admin Dashboard Starter Kit</h1>
+      <p className="py-2 mt-4">
+        ✓ <span className="font-normal">Management Produk</span>
+      </p>
+      <p className="py-2">
+        ✓ <span className="font-normal">Cek iwayat barang terjual</span>
+      </p>
+      <p className="py-2">
+        ✓ <span className="font-normal">Cek ketersediaan barang</span>
+      </p>
+    </>
+  );
+}
+
+export default TemplatePointers;
